5db5d3f87945747c481a1062cd694fe6cc3abfc7
felix <sparkyxu@hotmail.com>
2018-11-11 17:20:21 -0800

    footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -6,17 +6,44 @@
  */
 
 ?>
+<html>
+	<body>
+		 <!-- #content -->
 
-			</div><!-- #content -->
-
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+	  <footer id="colophon" class="site-footer" role="contentinfo">
+         <div class="site-info">
+			<div class="contact-info">
+		           <p>
+			          <i class="fas fa-mobile-alt"></i>
+			          <a href="tel:7784567891">778-456-7891</a>
+		          </p>
+		           <p>
+			           <i class="fas fa-envelope"></i>
+    		           <a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+		           </p>
+		           <p>
+			           <span>
+				           <i class="fab fa-facebook-square"></i>
+			        </span>
+    		          <span>
+				        <i class="fab fa-twitter-square"></i>
+			         </span>
+			       <span>
+				<i class="fab fa-google-plus-square"></i>
+			</span>
+		  </p>
+	               </div>
+			<div class="business-hours">
+			       <h3>Business Hours</h3>
+	            <span class="week-day">Monday-Friday: 9am to 5pm</span><br>
+	            <span class="week-day">Saturday: 10am to 2pm</span><br>
+	            <span class="week-day">Sunday: Closed</span><br>
+                   </div>  <!--business-hours-->
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
-		</div><!-- #page -->
+		  </div><!-- #page -->
 
 		<?php wp_footer(); ?>
 
-	</body>
+    </body>
 </html>
