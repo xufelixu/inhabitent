f4cdf942ca0ac549ece81d0366b969ab38014bfe
felix <sparkyxu@hotmail.com>
2018-11-13 23:43:30 -0800

    update Journal

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -42,15 +42,6 @@ get_header(); ?>
 </article><!-- #post-## -->
 
 
-
-
-
-
-
-
-
-
-
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
 			<?php the_post_navigation(); ?>
@@ -67,5 +58,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
