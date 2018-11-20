2980d0c415686730f24196de42cdb318ef5b95b3
felix <sparkyxu@hotmail.com>
2018-11-19 21:30:19 -0800

    try to fix the missing stylesheet problem by reloading the theme from github that commit before the lunch good luck

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,12 +1,11 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	
 <label>
-  <input style="font-size:12px;color:blue;padding:5px 20px;text-align:left;" 
-  type="search" class="search-field" placeholder="TYPE AND HIT ENTER ....." 
+  <input type="search" class="search-field" placeholder="TYPE AND HIT ENTER ....." 
   value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 </label>
 
-	<button style="color:#248483;background:white;" class="search-submit">
+	<button class="search-submit" type="submit">
 		<span class="icon-search" aria-hidden="true">
 			<i class="fa fa-search"></i>
 		</span>
